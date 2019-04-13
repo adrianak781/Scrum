2942bd165df14fde0fbc564a7a8df3629bc5d2d9
Arek <amitura@protonmail.com>
2019-04-13 15:32:49 +0200

    In Greetings class greet method for multiply names in name value split by ",".

--- src/main/java/pl/sda/scrum/exercises/Greetings.java
+++ src/main/java/pl/sda/scrum/exercises/Greetings.java
@@ -3,14 +3,27 @@ package pl.sda.scrum.exercises;
 public class Greetings {
 
     public String greet(String name) {
-        String result;
+        String result = "";
 
         if (name != null) {
+            if(name.contains(",")){
+                String[] names = name.split(",");
+                result = names[0];
+                for (int i = 1; i < names.length; i++) {
+                    result = result + "," + names[i];
+                    if(i == names.length-1){
+                        result= result + " i " + names[i];
+                    }
+                }
+                return result + ", witajcie!";
+
+            }
             if (name == name.toUpperCase()){
                 result = "WITAJ, " + name + "!";
             }else {
                 result = "Witaj, " + name + "!";
             }
+
         } else {
             result = "Witaj, mój przyjacielu";
         }
