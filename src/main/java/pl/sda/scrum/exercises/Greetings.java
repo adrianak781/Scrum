a87d2018d1281a7548997010f796f24f7bd27902
Arek <amitura@protonmail.com>
2019-04-13 15:08:12 +0200

    In Greetings class greet method security for name null value.

--- src/main/java/pl/sda/scrum/exercises/Greetings.java
+++ src/main/java/pl/sda/scrum/exercises/Greetings.java
@@ -2,8 +2,11 @@ package pl.sda.scrum.exercises;
 
 public class Greetings {
 
-    public String greet(String name){
-
-        return "Witaj, " + name + "!";
+    public String greet(String name) {
+        if (name != null) {
+            return "Witaj, " + name + "!";
+        } else {
+            return "Witaj, mój przyjacielu";
+        }
     }
 }
