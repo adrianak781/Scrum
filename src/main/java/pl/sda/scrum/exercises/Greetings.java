10237858fe5aa6a81e090c77ef9f18d40b809eaf
Adriana Kasum <kasumi781.ak@gmail.com>
2019-04-13 14:54:40 +0200

    Initial commit

---
+++ src/main/java/pl/sda/scrum/exercises/Greetings.java
@@ -0,0 +1,9 @@
+package pl.sda.scrum.exercises;
+
+public class Greetings {
+
+    public String greet(String name){
+
+        return null;
+    }
+}
---
+++ src/test/java/pl/sda/scrum/exercises/GreetingsTest.java
@@ -0,0 +1,19 @@
+package pl.sda.scrum.exercises;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class GreetingsTest {
+
+    @Test
+    public void greet() {
+        //given
+        Greetings greetings = new Greetings();
+        String name = "Adam;";
+        //when
+        String result = greetings.greet(name);
+        //then
+        assertEquals("Witaj, Adam!", result);
+    }
+}
